aebb281ab0c4257b963940482b8d8962ad71dc58
grigor-aramyan <grigori.aramyan@gmail.com>
2020-05-16 03:03:30 +0400

    tmp edit of project settings

--- generaitor_ml_api/settings.py
+++ generaitor_ml_api/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'generaitor_ml_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

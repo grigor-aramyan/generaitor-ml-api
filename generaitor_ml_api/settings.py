9a4fc8c06677e9cc2a6775391908b4d511ed733f
grigor-aramyan <grigori.aramyan@gmail.com>
2020-05-16 03:07:12 +0400

    remove path to static files

--- generaitor_ml_api/settings.py
+++ generaitor_ml_api/settings.py
@@ -113,6 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 django_heroku.settings(locals())

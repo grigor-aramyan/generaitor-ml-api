d479903d1c77b23b57b19354e4921efa8d94e385
grigor-aramyan <grigori.aramyan@gmail.com>
2020-05-16 03:46:10 +0400

    add Feedback model and migration

--- summarizer/models.py
+++ summarizer/models.py
@@ -8,3 +8,15 @@ class FeedbacksSum(models.Model):
     summary = models.TextField()
     insert_date = models.DateTimeField(default=timezone.now)
     update_date = models.DateTimeField(default=timezone.now)
+
+
+class Feedback(models.Model):
+    organization_name = models.CharField(blank=False, max_length=255)
+    for_product = models.BooleanField()
+    product_id = models.BigIntegerField()
+    branch_address = models.TextField()
+    sentiment = models.IntegerField()
+    keywords = models.CharField(blank=False, max_length=255)
+    content = models.TextField()
+    feedback_sum = models.ForeignKey(FeedbacksSum, on_delete=models.CASCADE)
+    
